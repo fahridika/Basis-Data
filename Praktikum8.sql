a7d486034693d2243cb0b989ae9750204264322d
Sindu Masri Priandana <73923369+SinduMP@users.noreply.github.com>
2022-05-25 13:47:21 +0700

    Create Praktikum8.sql

---
+++ Praktikum8.sql
@@ -0,0 +1,15 @@
+Create Table pemeran (
+kode_pem Char(5) Primary Key Unique Not Null,
+nama Char(20) Not Null,
+tgl_lahir Date Not Null,
+sex Char(1) Not Null);
+
+Create Table film (
+kode_film Char(5) Primary Key Unique Not Null,
+judul Char(30) Not Null,
+tahun Char(4) Not Null);
+
+Create Table mainsbg (
+kode_pem Char(5) Unique Not Null,
+kode_film Char(5) Unique Not Null,
+peran Char(20) Not Null);
